5cec906b03e93e8d5264b62232f1638e105235a2
Patel Smit <108448105+SMIT10122002@users.noreply.github.com>
2026-02-18 16:32:50 +0000

    datatypes done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/*
+
+string
+number => 2 to power 53
+boolean => true/false
+bigint
+null
+undefined
+symbol => unique
+
+object
+*/
+
+console.log(typeof(2));
+console.log(typeof null);
+console.log(typeof undefined);
+
