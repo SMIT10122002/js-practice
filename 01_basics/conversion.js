15bb30d237b124ab3e2dd05c8a41e757f4dca0a9
Patel Smit <108448105+SMIT10122002@users.noreply.github.com>
2026-02-19 04:02:43 +0000

    done conversion

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -16,3 +16,32 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// operations
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = " Smit"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true);
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+
+
